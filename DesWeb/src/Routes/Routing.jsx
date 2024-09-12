721792ec381ec3dc8b7089889d8f4e582bb52ede
Michelle-0793 <mlopez@fwdcostarica.com>
2024-09-12 15:59:51 -0600

    Avances página home

--- DesWeb/src/Routes/Routing.jsx
+++ DesWeb/src/Routes/Routing.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from '../pages/Login';
 import Home from "../Pages/Home";
+import About from "../Pages/About";
 import Contact from "../Pages/Contact";
 import ProtectedRoute from '../ProtectedRoute'; 
 
@@ -10,8 +11,10 @@ const Routing = () => {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/Login" element={<ProtectedRoute><Login /></ProtectedRoute>}/>
+        <Route path="/About" element={<About />} />
         <Route path="/Contact" element={<Contact />} />
+        <Route path="/Contact" element={<Contact />} />
+        <Route path="/Login" element={<ProtectedRoute><Login /></ProtectedRoute>}/>
 
       </Routes>
     </Router>
